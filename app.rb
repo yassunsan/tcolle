1f6c93e4ad06f8321bc5cbbfa26fa9914629c736
yassunsan <yassunsan@users.noreply.github.com>
2019-03-12 00:37:26 +0900

    Initial commit
    とりあえず、画像URLの取得と表示を実装

---
+++ app.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+require "sinatra/reloader"
+require "twitter"
+require "dotenv"
+
+Dotenv.load
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV["CONSUMER_KEY"]
+  config.consumer_secret = ENV["CONSUMER_SECRET"]
+  config.access_token = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+end
+
+get '/' do
+  erb :index
+end
+
+get '/:twittername' do |name|
+  user_tweets = client.user_timeline(name, { count: 100 } )
+  @image_urls = user_tweets.flat_map { |s| s.media}.map { |m| m.media_url.to_s}
+  erb :result
+end
