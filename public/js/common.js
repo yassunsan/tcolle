08bafc87c1fb452e67161c8d9b924992b6f18b42
yassunsan <yassun4110@gmail.com>
2019-03-20 18:49:15 +0900

    ページ最下部までスクロールした際に画像取得中画像を表示するようにした

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,21 @@
-$(window).on("load resize", function(){
+$(window).on("load resize", function () {
     $(".img-trim").height($(".img-trim").width());
+});
+
+$(function () {
+    $(window).bottom({
+        proximity: 0
+    });
+    $(window).bind("bottom", function () {
+        var obj = $(this);
+        if (!obj.data('loading')) {
+            obj.data('loading', true);
+            $('#loadimg').html('<img src="img/icon_loader.gif" />');
+            setTimeout(function () {
+                $('#loadimg').html('');
+                // TODO: 画像を追加で取得してくる処理を書く
+                obj.data('loading', false);
+            }, 1500);
+        }
+    });
 });
